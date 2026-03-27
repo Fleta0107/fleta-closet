fa7b37b58a7133c5508739dee0155e0b6d60da65
김민식 <kimminsik@gimminsig-ui-Macmini.local>
2026-03-27 23:18:26 +0900

    chore: improve project config (JpaConfig, .gitignore, yml comments)

--- backend/src/main/java/com/fleta/closet/FletaClosetApplication.java
+++ backend/src/main/java/com/fleta/closet/FletaClosetApplication.java
@@ -2,10 +2,8 @@ package com.fleta.closet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class FletaClosetApplication {
     public static void main(String[] args) {
         SpringApplication.run(FletaClosetApplication.class, args);
